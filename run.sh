d4d98246ff16fbdfe5a0560fde1411a5141359eb
Zhiyuan Yu <yuzhiyuan.jacky@qq.com>
2026-05-17 14:44:03 +0800

    新增批量实验脚本

---
+++ run.sh
@@ -0,0 +1,11 @@
+# /mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset cora --model basic_gnn
+# /mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset cora --model gcn
+# /mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset cora --model graph_transformer
+
+/mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset citeseer --model basic_gnn
+/mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset citeseer --model gcn
+/mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset citeseer --model graph_transformer
+
+/mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset pubmed --model basic_gnn
+/mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset pubmed --model gcn
+/mnt/HDD2/yzy23/.conda/envs/ocs/bin/python main.py --dataset pubmed --model graph_transformer
